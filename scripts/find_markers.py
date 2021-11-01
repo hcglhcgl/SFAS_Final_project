4d44806c6f080be34a5c48e46cef665c009a4ba8
kroghm96 <kroghm5@gmail.com>
2021-11-01 16:58:54 +0100

    Added feature that reads data from QR code through a subscription

--- scripts/find_markers.py
+++ scripts/find_markers.py
@@ -1,26 +1,17 @@
 #!/usr/bin/env python
 
-def retrieveQR(data):
-    finalWord = ["0", "0", "0", "0", "0"]
+import rospy
+from std_msgs.msg import String
 
-    x = float(data.split("\r\n")[0].split("=")[1])
-    y = float(data.split("\r\n")[1].split("=")[1])
-    x_next = float(data.split("\r\n")[2].split("=")[1])
-    y_next = float(data.split("\r\n")[3].split("=")[1])
-    N = int(data.split("\r\n")[4].split("=")[1])
-    L = data.split("\r\n")[5].split("=")[1]
 
-    finalWord[N-1] = L
+def sub_cal(data):
+    rospy.loginfo(data.data)
 
-    finalWord = ''.join(finalWord)
 
-    return x, y, x_next, y_next, N, L, finalWord
+def listener():
+	rospy.init_node('subscriberNode', anonymous=True)
+	rospy.Subscriber('/visp_auto_tracker/code_message', String, sub_cal)
+	rospy.spin()
 
-#The main script file
 if __name__ == '__main__':
-
-    data = "X=2.35\r\nY=3.24\r\nX_next=5.3\r\nY_next=5.9\r\nN=3\r\nL=M"
-
-    x, y, x_next, y_next, N, L, finalWord = retrieveQR(data)
-
-    print x, y, x_next, y_next, N, L, finalWord
+	listener()
