6969d9c83f56115fb78f861581e842590616e521
MagnusErler <magnus.erler@gmail.com>
2021-11-01 16:18:49 +0100

    Read QR-code

--- scripts/find_markers.py
+++ scripts/find_markers.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python
 
+
+#Read QR-code
+finalWord = ["0", "0", "0", "0", "0"]
+
+data = "X=2.35\r\nY=3.24\r\nX_next=5.3\r\nY_next=5.9\r\nN=3\r\nL=M"
+
+x = float(data.split("\r\n")[0].split("=")[1])
+y = float(data.split("\r\n")[1].split("=")[1])
+x_next = float(data.split("\r\n")[2].split("=")[1])
+y_next = float(data.split("\r\n")[3].split("=")[1])
+N = int(data.split("\r\n")[4].split("=")[1])
+L = data.split("\r\n")[5].split("=")[1]
+
+finalWord[N-1] = L
+
 #The main script file
