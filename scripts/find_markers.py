9df66416f4e0a4c67edf9de4ce1d223734b72fb6
MagnusErler <magnus.erler@gmail.com>
2021-11-01 16:29:07 +0100

    added function to retrieve qr code

--- scripts/find_markers.py
+++ scripts/find_markers.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python
 
+def retrieveQR(data):
+    finalWord = ["0", "0", "0", "0", "0"]
 
-#Read QR-code
-finalWord = ["0", "0", "0", "0", "0"]
+    x = float(data.split("\r\n")[0].split("=")[1])
+    y = float(data.split("\r\n")[1].split("=")[1])
+    x_next = float(data.split("\r\n")[2].split("=")[1])
+    y_next = float(data.split("\r\n")[3].split("=")[1])
+    N = int(data.split("\r\n")[4].split("=")[1])
+    L = data.split("\r\n")[5].split("=")[1]
 
-data = "X=2.35\r\nY=3.24\r\nX_next=5.3\r\nY_next=5.9\r\nN=3\r\nL=M"
+    finalWord[N-1] = L
 
-x = float(data.split("\r\n")[0].split("=")[1])
-y = float(data.split("\r\n")[1].split("=")[1])
-x_next = float(data.split("\r\n")[2].split("=")[1])
-y_next = float(data.split("\r\n")[3].split("=")[1])
-N = int(data.split("\r\n")[4].split("=")[1])
-L = data.split("\r\n")[5].split("=")[1]
+    finalWord = ''.join(finalWord)
 
-finalWord[N-1] = L
+    return x, y, x_next, y_next, N, L, finalWord
 
 #The main script file
+if __name__ == '__main__':
+
+    data = "X=2.35\r\nY=3.24\r\nX_next=5.3\r\nY_next=5.9\r\nN=3\r\nL=M"
+
+    x, y, x_next, y_next, N, L, finalWord = retrieveQR(data)
+
+    print x, y, x_next, y_next, N, L, finalWord
