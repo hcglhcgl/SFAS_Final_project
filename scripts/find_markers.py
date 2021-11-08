59b8073d6f39d26a8f443fe6e1f2abcdb1c9194c
MagnusErler <magnus.erler@gmail.com>
2021-11-08 21:35:46 +0100

    Start with spinning around until QR found

--- scripts/find_markers.py
+++ scripts/find_markers.py
@@ -3,16 +3,50 @@
 import rospy
 #import random_driving
 from std_msgs.msg import String
+from geometry_msgs.msg import Twist
 
 finalWord_list = ["0", "0", "0", "0", "0"]
 N_old = 0
+gotoNextQR = False
+
+cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+twist = Twist()
+
 
 def QRlistener():
     rospy.init_node('subscriberNode', anonymous=True)
-    rospy.Subscriber('/visp_auto_tracker/code_message', String, retrieveQR)
+    rospy.Subscriber('/visp_auto_tracker/code_message', String, getQR)
     rospy.spin()
 
-def retrieveQR(data):
+def objectPos():
+    print "here1"
+    rospy.init_node('subscriberNode', anonymous=True)
+    rospy.Subscriber('/visp_auto_tracker/object_position', String, getObjectPos)
+    rospy.spin()
+
+def getObjectPos(data):
+    print "here2"
+    rospy.loginfo(data.data)
+
+def spinRobot():
+    print "You spin me right round"
+
+    twist.angular.z = 0.4
+
+    cmd_vel_pub.publish(twist)
+
+def stopRobot():
+    print "Stopping robot"
+
+    twist.linear.x = 0.0
+    twist.angular.z = 0.0
+
+    cmd_vel_pub.publish(twist)
+
+def getQR(data):
+    global gotoNextQR
+    if not gotoNextQR:
+        spinRobot()
     if "0" in finalWord_list:
         if data.data:
             x = float(data.data.split("\r\n")[0].split("=")[1])
@@ -29,14 +63,19 @@ def retrieveQR(data):
                 N_old = N
                 print x, y, x_next, y_next, N, L, finalWord_list
 
-            #call function that moves the robot to next QR-code
+                print "call function that moves the robot to next QR-code"
+                stopRobot()
+                objectPos()
+                gotoNextQR = True
     else:
         finalWord_str = ''.join(finalWord_list)
         print "Done with exploring"
         print "Final word: " + finalWord_str
-        rospy.signal_shutdown("reason") 
+        rospy.signal_shutdown("reason")
+
 
 if __name__ == '__main__':
+    print "Starting program"
     rospy.init_node('subscriberNode', anonymous=True)
     rate = rospy.Rate(60)
     QRlistener()
