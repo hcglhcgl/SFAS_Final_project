4ef242e17037261b9eb35c0eb8bc826495c23ef3
MagnusErler <magnus.erler@gmail.com>
2021-11-08 20:47:28 +0100

    print finalWord when all found - then shutdown

--- scripts/find_markers.py
+++ scripts/find_markers.py
@@ -3,33 +3,37 @@
 import rospy
 from std_msgs.msg import String
 
-finalWord = ["0", "0", "0", "0", "0"]
+finalWord_list = ["0", "0", "0", "0", "0"]
+N_old = 0
+
+def QRlistener():
+    rospy.init_node('subscriberNode', anonymous=True)
+    rospy.Subscriber('/visp_auto_tracker/code_message', String, retrieveQR)
+    rospy.spin()
+
 def retrieveQR(data):
-    
-    x = float(data.split("\r\n")[0].split("=")[1])
-    y = float(data.split("\r\n")[1].split("=")[1])
-    x_next = float(data.split("\r\n")[2].split("=")[1])
-    y_next = float(data.split("\r\n")[3].split("=")[1])
-    N = int(data.split("\r\n")[4].split("=")[1])
-    L = data.split("\r\n")[5].split("=")[1]
+    if "0" in finalWord_list:
+        if data.data:
+            x = float(data.data.split("\r\n")[0].split("=")[1])
+            y = float(data.data.split("\r\n")[1].split("=")[1])
+            x_next = float(data.data.split("\r\n")[2].split("=")[1])
+            y_next = float(data.data.split("\r\n")[3].split("=")[1])
+            N = int(data.data.split("\r\n")[4].split("=")[1])
+            L = data.data.split("\r\n")[5].split("=")[1]
 
-    finalWord[N-1] = L
+            finalWord_list[N-1] = L
+            
+            global N_old
+            if N_old is not N:
+                N_old = N
+                print x, y, x_next, y_next, N, L, finalWord_list
 
-    return x, y, x_next, y_next, N, L
-
-
-def listener():
-	rospy.init_node('subscriberNode', anonymous=True)
-	rospy.Subscriber('/visp_auto_tracker/code_message', String, sub_cal)
-	rospy.spin()
-
-def sub_cal(data):
-    if data.data:
-        x, y, x_next, y_next, N, L = retrieveQR(data.data)
-
-        print x, y, x_next, y_next, N, L, finalWord
-
-        #call function that moves the robot to next QR-code
+            #call function that moves the robot to next QR-code
+    else:
+        finalWord_str = ''.join(finalWord_list)
+        print "Done with exploring"
+        print "Final word: " + finalWord_str
+        rospy.signal_shutdown("reason") 
 
 if __name__ == '__main__':
-    listener()
+    QRlistener()
